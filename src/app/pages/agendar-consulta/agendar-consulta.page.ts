ded57439ce72e6858058a4479e675cc244266aa6
John Taylor de Miranda Lobato <john13miranda@gmail.com>
2022-11-15 01:53:20 -0300

    Master

--- src/app/pages/agendar-consulta/agendar-consulta.page.ts
+++ src/app/pages/agendar-consulta/agendar-consulta.page.ts
@@ -1,4 +1,6 @@
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-agendar-consulta',
@@ -6,10 +8,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./agendar-consulta.page.scss'],
 })
 export class AgendarConsultaPage implements OnInit {
+  hourValues: string;
+  minuteValues: string;
+  procedimento: any;
+  procedimentoSel: any;
+  profissional: any;
+  profissionalSel: any;
 
-  constructor() { }
 
-  ngOnInit() {
+  constructor(
+    private firestore: AngularFirestore,
+    private router: Router
+    )
+    {
+    this.hourValues = '08,09,10,11,13,14,15,16';
+    this.minuteValues = '00,30';
+    this.procedimento = this.firestore.collection('procedimento').valueChanges();
+    this.profissional = this.firestore.collection('profissional').valueChanges();
+
   }
 
+
+  ngOnInit() {}
+  isWeekday = (dateString: string) => {
+    const date = new Date(dateString);
+    const utcDay = date.getUTCDay();
+
+    return utcDay !== 0 && utcDay !== 6;
+  };
+  canSave(): boolean {
+    return this.profissionalSel != null && this.procedimentoSel != null;
+  }
+
+  config(servico, agente, data){
+    const nowdate = new Date(data.value).toLocaleDateString();
+    const nowtime = new Date(data.value).toLocaleTimeString();
+    this.router.navigate(['agendar-consulta2'],{queryParams:[servico.value,agente.value,nowdate,nowtime]});
+  }
 }
--- src/app/pages/agendar-consulta2/agendar-consulta2.page.ts
+++ src/app/pages/agendar-consulta2/agendar-consulta2.page.ts
@@ -1,4 +1,7 @@
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-agendar-consulta2',
@@ -7,7 +10,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AgendarConsulta2Page implements OnInit {
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute) {
+    this.activatedRoute.queryParams.subscribe(params => {
+      console.log('sevico: ',params[0]);
+      console.log('agente: ',params[1]);
+      console.log('nowdata: ',params[2]);
+      console.log('nowtime: ',params[3]);
+    });
+
+   }
 
   ngOnInit() {
   }
