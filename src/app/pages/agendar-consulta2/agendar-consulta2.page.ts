bba81e0b829c431c004e91bdff6698e3ed5b6296
John Taylor de Miranda Lobato <john13miranda@gmail.com>
2022-11-15 22:33:00 -0300

    Master

--- src/app/pages/agendar-consulta2/agendar-consulta2.page.ts
+++ src/app/pages/agendar-consulta2/agendar-consulta2.page.ts
@@ -9,18 +9,36 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./agendar-consulta2.page.scss'],
 })
 export class AgendarConsulta2Page implements OnInit {
+  agente: any;
+  service: any;
+  nowdata: any;
+  nowtime: any;
+  nomeSel: any;
+  sobrenomeSel: any;
+  idadeSel: any;
+  datadenascimentoSel: any;
 
-  constructor(private activatedRoute: ActivatedRoute) {
+  constructor(private activatedRoute: ActivatedRoute, private router: Router) {
     this.activatedRoute.queryParams.subscribe(params => {
       console.log('sevico: ',params[0]);
       console.log('agente: ',params[1]);
       console.log('nowdata: ',params[2]);
       console.log('nowtime: ',params[3]);
+
+      this.service = params[0];
+      this.agente = params[1];
+      this.nowdata = params[2];
+      this.nowtime = params[3];
     });
 
    }
 
   ngOnInit() {
   }
-
+  canSave(): boolean {
+    return this.nomeSel != null && this.sobrenomeSel != null && this.idadeSel != null && this.datadenascimentoSel != null;
+  }
+  config(nome,sobrenome,idade,datadenascimento){
+    this.router.navigate(['pagamento'],{queryParams:[this.service,this.agente,this.nowdata,this.nowtime,nome.value,sobrenome.value,idade.value,datadenascimento.value]});
+  }
 }
--- src/app/pages/pagamento/pagamento.page.ts
+++ src/app/pages/pagamento/pagamento.page.ts
@@ -1,4 +1,7 @@
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-pagamento',
@@ -6,10 +9,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pagamento.page.scss'],
 })
 export class PagamentoPage implements OnInit {
+  agente: any;
+  service: any;
+  nowdata: any;
+  nowtime: any;
+  nome: any;
+  sobrenome: any;
+  idade: any;
+  datadenascimento: any;
+  pagamentoSel: any;
+  pagamentos: any;
+  agendamentos: any;
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute, private router: Router, private firestore: AngularFirestore) {
+
+    this.activatedRoute.queryParams.subscribe(params => {
+      console.log('sevico: ',params[0]);
+      console.log('agente: ',params[1]);
+      console.log('nowdata: ',params[2]);
+      console.log('nowtime: ',params[3]);
+      console.log('nome: ', params[4]);
+      console.log('sobrenome: ', params[5]);
+      console.log('idade: ', params[6]);
+      console.log('datadenascimento: ', params[7]);
+
+      this.service = params[0];
+      this.agente = params[1];
+      this.nowdata = params[2];
+      this.nowtime = params[3];
+      this.nome = params[4];
+      this.sobrenome = params[5];
+      this.idade = params[6];
+      this.datadenascimento = params[7];
+
+      this.pagamentos =  this.firestore.collection('pagamentos').valueChanges();
+      this.agendamentos = firestore.collection('agendamentos');
+    });
+
+
+   }
 
   ngOnInit() {
   }
+  checkValue(event) {
+    this.pagamentoSel = event.detail.value;
+  }
+  agendar(){
+    this.agendamentos.add({service: this.service,agente: this.agente, data: this.nowdata, horario: this.nowtime, nomePaciente: this.nome, sobrenomePaciente: this.sobrenome, idadePaciente: this.idade, nascimentoPaciente: this.datadenascimento,pagamento: this.pagamentoSel,id: ''}).then( novo => {
+      this.agendamentos.doc(novo.id).update({id: novo.id})
+      });
 
+    this.router.navigateByUrl('historico-consulta');
+  }
 }
